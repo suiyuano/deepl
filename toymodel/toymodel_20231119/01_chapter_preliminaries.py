a341c4de6e485ea3fdde46177da586c333aea155
suiyuano <1095529505@qq.com>
2023-11-22 23:58:00 +0800

    update

--- toymodel/toymodel_20231119/01_chapter_preliminaries.py
+++ toymodel/toymodel_20231119/01_chapter_preliminaries.py
@@ -121,9 +121,28 @@ def calculus_learn():
     plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
 
 
+def autograd():
+    x = torch.arange(4.0)
+    print(x)
+    x.requires_grad_(True)  # 等价于x=torch.arange(4.0,requires_grad=True)
+    print(x.grad)  # 默认值是None
+    y = 2 * torch.dot(x, x)
+    print(y)
+    y.backward()
+    print(x.grad)
+    print(x.grad == 4 * x)
+
+    # 在默认情况下，PyTorch会累积梯度，我们需要清除之前的值
+    x.grad.zero_()
+    y = x.sum()
+    y.backward()
+    print(x.grad)
+
+
 def main():
     # pandas_learn()
-    calculus_learn()
+    # calculus_learn()
+    autograd()
 
 
 # Press the green button in the gutter to run the script.
