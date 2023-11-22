9aa646c961dbe29b0a8a9640bb9581cc11093932
suiyuano <1095529505@qq.com>
2023-11-23 00:07:14 +0800

    update

--- toymodel/toymodel_20231119/01_chapter_preliminaries.py
+++ toymodel/toymodel_20231119/01_chapter_preliminaries.py
@@ -138,6 +138,14 @@ def autograd():
     y.backward()
     print(x.grad)
 
+    # 对非标量调用backward需要传入一个gradient参数，该参数指定微分函数关于self的梯度。
+    # 本例只想求偏导数的和，所以传递一个1的梯度是合适的
+    x.grad.zero_()
+    y = x * x
+    # 等价于y.backward(torch.ones(len(x)))
+    y.sum().backward()
+    print(x.grad)
+
 
 def main():
     # pandas_learn()
