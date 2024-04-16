313ae6b04c935d8a8a7b9e7dea57db35fb8b94bd
suiyuano <1095529505@qq.com>
2024-04-17 00:14:04 +0800

    new addition

--- d2l_local/chapter_preliminaries_autograd.py
+++ d2l_local/chapter_preliminaries_autograd.py
@@ -30,3 +30,76 @@ print(y)
 y.sum().backward()
 # y.backward()  #这个会报错
 print(x.grad)
+
+x.grad.zero_()
+y = x * x
+u = y.detach()
+z = u * x
+z.sum().backward()
+print(x.grad == u)
+
+x.grad.zero_()
+y.sum().backward()
+print(x.grad == 2 * x)
+
+def f(a):
+    b = a * 2
+    while b.norm() < 1000:
+        b = b * 2
+    if b.sum() > 0:
+        c = b
+    else:
+        c = 100 * b
+    return c
+
+a = torch.randn(size=(), requires_grad=True)
+d = f(a)
+d.backward()
+
+print(a.grad == d / a)
+
+# practice
+print('''-----Practice-----''')
+
+# practice2
+x.grad.zero_()
+y = x * x
+print(y)
+# 等价于y.backward(torch.ones(len(x)))
+y.sum().backward()
+# y.backward()  #这个会报错
+print(x.grad)
+# y.sum().backward()  #马上再次求导会报错
+
+# practice5
+import torch
+
+# x.grad.zero_()
+
+# x = torch.rand(2, 4, requires_grad = True)
+x = torch.linspace(0, 2 * torch.pi, 100,  requires_grad = True)  # 生成100
+y = torch.sin(x)
+y.sum().backward()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib_inline import backend_inline
+
+plt.figure(figsize=(8, 8))
+
+plt.plot(x.detach().numpy(), y.detach().numpy(), "-", label="sin(x)")
+plt.plot(x.detach().numpy(), x.grad.detach().numpy(), "r--", label="cos(x)")
+
+# x1 = torch.linspace(0, 2 * torch.pi, 200)
+# plt.plot(x1.numpy(), torch.zeros_like(x1).numpy(), "--", label="0")
+
+plt.legend(loc='upper left')
+
+# 添加标签和标题
+plt.xlabel('X')
+plt.ylabel('f(x)')
+
+# 显示图表
+
+plt.show()
