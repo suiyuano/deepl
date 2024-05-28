436eeafd4263cf99388d6b7737aa8cbdbf34fc26
suiyuano <1095529505@qq.com>
2024-05-28 23:20:52 +0800

    new addition

---
+++ d2l_local/chapter_multilayer-perceptrons_numerical-stability-and-init.py
@@ -0,0 +1,20 @@
+# %matplotlib inline
+import torch
+from d2l import torch as d2l
+
+x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+y = torch.sigmoid(x)
+y.backward(torch.ones_like(x))
+
+d2l.plot(x.detach().numpy(), [y.detach().numpy(), x.grad.numpy()],
+         legend=['sigmoid', 'gradient'], figsize=(4.5, 2.5))
+
+
+d2l.plt.show()
+
+M = torch.normal(0, 1, size=(4,4))
+print('一个矩阵 \n',M)
+for i in range(100):
+    M = torch.mm(M,torch.normal(0, 1, size=(4, 4)))
+
+print('乘以100个矩阵后\n', M)
